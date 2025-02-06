2e5f44a393353566f17b80382e144a625a7527c2
EleCTRoNaTiCs <electronatics02@gmail.com>
2025-02-06 15:55:47 +0800

    can it be

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "sql12.freesqldatabase.com";
-$username = "sql12761433";
-$password = "JsW8TZdP62";
-$dbname = "sql12761433";
+$servername = $url["sql12.freesqldatabase.com"];
+$username = $url["sql12761433"];
+$password = $url["JsW8TZdP62"];
+$dbname = $url["sql12761433"];
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
