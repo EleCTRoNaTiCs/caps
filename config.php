c6a83ffd479cdb870136688eeb5876ed44b5e409
EleCTRoNaTiCs <electronatics02@gmail.com>
2025-02-06 15:30:15 +0800

    try

--- config.php
+++ config.php
@@ -1,9 +1,8 @@
 <?php
-define(constant_name: 'SITEURL', value: 'http://localhost/caps/');
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "beyond_session ";
+$servername = "sql12.freesqldatabase.com";
+$username = "sql12761433";
+$password = "JsW8TZdP62";
+$dbname = "sql12761433";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
