68aea7d30ffae19fdef5c2c87506576d6c6829e2
EleCTRoNaTiCs <electronatics02@gmail.com>
2025-02-06 15:48:55 +0800

    c

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $username = "sql12761433";
 $password = "JsW8TZdP62";
 $dbname = "sql12761433";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
